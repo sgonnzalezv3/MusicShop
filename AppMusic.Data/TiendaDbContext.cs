d5b4076fca87e6a27b3f060094105d12a7ef987e
sgonnzalezv3 <60562811+sgonnzalezv3@users.noreply.github.com>
2021-04-07 01:24:07 -0500

    Add files via upload

---
+++ AppMusic.Data/TiendaDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AppMusic.Data.Configuracion;
+using AppMusic.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace AppMusic.ApiWeb
+{
+    public partial class TiendaDbContext : DbContext
+    {
+        public TiendaDbContext(DbContextOptions<TiendaDbContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<DetalleOrden> DetallesOrden { get; set; }
+        public virtual DbSet<Orden> Ordenes { get; set; }
+        public virtual DbSet<Perfil> Perfiles { get; set; }
+        public virtual DbSet<Producto> Productos { get; set; }
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+            //Llamando la clase que se ha creado para ordenamiento efiiente
+            modelBuilder.ApplyConfiguration(new DetalleOrdenConfiguracion());
+            modelBuilder.ApplyConfiguration(new OrdenConfiguracion());
+            modelBuilder.ApplyConfiguration(new PerfilConfiguracion());
+            modelBuilder.ApplyConfiguration(new ProductoConfiguracion());
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguracion());
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
